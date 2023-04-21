a5a9608e7218fb8aa18a7c3f2fcddb50a815780a
Neil Schelly <neil@neilschelly.com>
2023-04-21 10:45:12 -0400

    Adding basic metrics exporter

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"os"
+	"net/http"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/neilschelly/s3awslogbeat/beater"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var Version = "0.0.3"
+var Version = "0.1.0"
 var Name = "cloudtrailbeat"
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":9400", nil)
+
 	if err := beat.Run(Name, Version, beater.New()); err != nil {
 		os.Exit(1)
 	}
